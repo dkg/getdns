8da914edb7850a6b05ef5f293088a13d514339f7
saradickinson <sara@sinodun.com>
2015-07-19 13:18:38 +0200

    add new auth name to context

--- src/context.h
+++ src/context.h
@@ -101,6 +101,8 @@ typedef struct getdns_upstream {
 	getdns_eventloop_event   event;
 	getdns_eventloop        *loop;
 	getdns_tcp_state         tcp;
+	char                     tls_auth_name[GETDNS_MAX_DNAME_LEN + 1];
+	
 
 	/* Pipelining of TCP network requests */
 	getdns_network_req      *write_queue;
